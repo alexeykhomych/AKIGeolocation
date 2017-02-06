3b40a8c53e69354e805e8bd2376bcbf01335e851
Alexey Khomych <alexeyfurious@gmail.com>
2017-02-06 12:56:30 +0200

    added fb context

---
+++ Geolocation/Library/Classes/Models/AKIContext/AKIFacebookContext/AKIFacebookLoginContext/AKIFacebookLoginContext.swift
@@ -0,0 +1,15 @@
+//
+//  AKIFacebookLoginContext.swift
+//  Geolocation
+//
+//  Created by Alexey Khomych on 2/6/17.
+//  Copyright © 2017 Alexey Khomych. All rights reserved.
+//
+
+import UIKit
+
+class AKIFacebookLoginContext: AKIContext {
+    
+    func 
+    
+}
