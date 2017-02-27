ea55d5c557d9a795df3b44ae5ec366ed1f131e22
Alexey Khomych <alexeyfurious@gmail.com>
2017-02-27 17:30:04 +0200

    added locationManager and ServiceTimerProvider

---
+++ Geolocation/Source/Models/AKIServiceTimerProvider.swift
@@ -0,0 +1,13 @@
+//
+//  AKIServiceTimerProvider.swift
+//  Geolocation
+//
+//  Created by Alexey Khomych on 2/27/17.
+//  Copyright © 2017 Alexey Khomych. All rights reserved.
+//
+
+import UIKit
+
+class AKITimer {
+
+}
